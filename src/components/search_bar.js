e71a6674becc1841c7fd7955da780b6c54c3c703
= <=>
2018-06-14 19:36:47 +0530

    Added search functioality.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // Above line equals to.
 // const component = React.Component
@@ -10,12 +10,12 @@ import React from 'react';
 ////////////////////////////////
 
 // ES6 Class baased controlled component.
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   // Constructor
   constructor(props) {
     super(props);
 
-    this.state = { term: 'Search' };
+    this.state = { term: 'animals' };
   }
 
   // Render method for class.
@@ -29,10 +29,15 @@ class SearchBar extends React.Component {
         <input
           className='search-bar react-search'
           value={ this.state.term }
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
 
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchChange(term);
   }
 
   // // Function to handle event of onChange.
